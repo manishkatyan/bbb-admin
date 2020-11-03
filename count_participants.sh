58b97707e1d07bfaafefda676e02447d7aec0147
Manish Katyan <manishkatyan@gmail.com>
2020-11-03 14:34:54 +0530

    Extract IP and user names

--- count_participants.sh
+++ count_participants.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+# Number of days of recordings for which participants counts need to be extracted
+NUM_DAYS=3
+
 MEETINGS="meeting_file.txt"
 
-find /var/bigbluebutton/recording/raw/ -maxdepth 1 -mtime -1 -printf "%f\n" | egrep "^[0-9a-f]{40}-[[:digit:]]{13}$" > "$MEETINGS"
+find /var/bigbluebutton/recording/raw/ -maxdepth 1 -mtime "$NUM_DAYS" -printf "%f\n" | egrep "^[0-9a-f]{40}-[[:digit:]]{13}$" > "$MEETINGS"
 
 while read meeting; do
   events_xml="/var/bigbluebutton/recording/raw/$meeting/events.xml"
