97cdb2b6b0c47913b7598bbe9a33993691c0e1f2
Manish Katyan <manishkatyan@gmail.com>
2020-11-11 10:16:58 +0530

    Meeting participants

--- meeting_analytics.sh
+++ meeting_analytics.sh
@@ -3,7 +3,7 @@
 MEETING_ID=$1
 
 echo "**"
-echo "Meeting Summary"
+echo "Meeting Analytics"
 
 if [[ -n $(find /var/bigbluebutton/recording/raw/ -maxdepth 1 -printf "%f\n" | grep -w "$MEETING_ID") ]]
 then
@@ -27,24 +27,24 @@ then
     BBB_CONTEXT=`xmlstarlet sel -t -v '//recording/metadata/@bbb-context' "$events_xml"`
     BBB_ORIGIN_SERVER_NAME=`xmlstarlet sel -t -v '//recording/metadata/@bbb-origin-server-name' "$events_xml"`
     MEETING_NAME=`xmlstarlet sel -t -v '//recording/metadata/@meetingName' "$events_xml"`
-    PARTICIPANT_COUNT=`xmlstarlet sel -t -v '//recording/event[@module="PARTICIPANT"]/name' "$events_xml" | sort --unique | wc -l`
-   
-    USER_INFO=`xmlstarlet sel -t -m '//recording/event[@eventname="ParticipantJoinEvent"][1]' -v "date" -o "|" -v "name" -o "|" -v "role" "$events_xml"`
-    START_TIME=$(cut -d'|' -f1 <<< "$USER_INFO")
-    NAME=$(cut -d'|' -f2 <<< "$USER_INFO")
-    ROLE=$(cut -d'|' -f3 <<< "$USER_INFO")
- 
 
+    PARTICIPANTS=`xmlstarlet sel -t -m '//recording/event[@eventname="ParticipantJoinEvent"]' -v "name" -o "," -v "role" -o "," -v "date" -n "$events_xml"`
+    
+    PARTICIPANTS_COUNT=`xmlstarlet sel -t -v '//recording/event[@eventname="ParticipantJoinEvent"]/name' "$events_xml" | sort | uniq | wc -l`
+    
+    START_TIME=`xmlstarlet sel -t -v '//recording/event[@eventname="ParticipantJoinEvent"][1]/date' "$events_xml"`
+    
     END_TIME=`xmlstarlet sel -t -v '//recording/event[@eventname="EndAndKickAllEvent"]/date' "$events_xml"`
  
     echo "bbb-context: $BBB_CONTEXT"
     echo "bbb-server-name: $BBB_ORIGIN_SERVER_NAME"
     echo "meeting name: $MEETING_NAME"
-    echo "participant count: $PARTICIPANT_COUNT"
+    echo "participant count: $PARTICIPANTS_COUNT"
     echo "start time: $START_TIME"
     echo "end time: $END_TIME"
-    echo "name: $NAME"
-    echo "role: $ROLE"
+
+    echo "Participants:"
+    echo "$PARTICIPANTS"
   
   else 
     echo "events.xml not found"
